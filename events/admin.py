9a764cc5f3ef671cd469c986c470014d0d27e6a6
Ben <benbakhshi@gmail.com>
2013-03-18 20:02:30 +0200

    Add list_display to Events admin and date_hierarchy.

--- events/admin.py
+++ events/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from events.models import Event
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+from django.contrib.contenttypes import generic
 
 
 class EventAdmin(admin.ModelAdmin):
-    pass    
 
+    list_display = ('when', 'what',)
+    # content_object = generic.GenericForeignKey("content_type", "object_id")
+    date_hierarchy = 'when'
 
-admin.site.register(Event, EventAdmin)
+admin.site.register(Event, EventAdmin, )
